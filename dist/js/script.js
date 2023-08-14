27e11cde1bb8fe2d60dfca5ac33f0bfb5e5b4da6
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2023-08-14 15:36:35 +0300

    added new sections
    -donate
    -emotions
    -footer
    -partners
    -plan
    -privacy
    -project
    -team
    -ukraininans

--- dist/js/script.js
+++ dist/js/script.js
@@ -12,6 +12,31 @@ document.querySelector('.header-wrapper__logo').addEventListener('mouseover', fu
     document.querySelector('.header-wrapper__blur').style.zIndex = '10';
 }, false);
 
+const slider = tns({
+    container: '.my-slider',
+    items: 4,
+    slideBy: 'page',
+    controls: false,
+    autoplay: false,
+    mouseDrag: true,
+});
+// var slider = tns({
+//     container: '.my-slider',
+//     items: 1,
+//     responsive: {
+//         640: {
+//             edgePadding: 20,
+//             gutter: 20,
+//             items: 2
+//         },
+//         700: {
+//             gutter: 30
+//         },
+//         900: {
+//             items: 3
+//         }
+//     }
+// });
 
 
 // let mediaQuery = window.matchMedia('(max-width: 768px)');
